f3eca6b61b1da83168bf09f70b82e4b38af72521
sudirboyy <sudirboyy@gmail.com>
2023-05-26 20:27:30 +0700

    15

--- Doublelinkedlist_178/Doublelinkedlist_178.cpp
+++ Doublelinkedlist_178/Doublelinkedlist_178.cpp
@@ -150,7 +150,6 @@ void DoubleLinkedList::revtransverse() {
     }
 }
 
-
 void DoubleLinkedList::hapus() {
     if (listEmpty())
     {
@@ -167,6 +166,26 @@ void DoubleLinkedList::hapus() {
     else
     {
         cout << "Record with roll number " << rollNo << " deleted" << endl;
+    }
+}
+
+void DoubleLinkedList::searchData() {
+    if (listEmpty() == true) {
+        cout << "\nList is empty" << endl;
+    }
+    Node* prev, * curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of the students whose record you want to search: ";
+    int num;
+    cin >> num;
+    if (DoubleLinkedList::search(num, &prev, &curr) == false) {
+        cout << "\nRecord not found" << endl;
+    }
+    else
+    {
+        cout << "\nRecord Found" << endl;
+        cout << "\nRoll number: " << curr->noMhs << endl;
+        cout << "\nName: " << curr->name << endl;
     }
 }
 
